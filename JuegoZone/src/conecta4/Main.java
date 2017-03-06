8e00c985f14573dac325264a05147565f9ad8487
abigailselles@hotmail.com <Abigail@miequipo>
2017-03-06 16:35:55 +0100

    clase main implementada

--- JuegoZone/src/conecta4/Main.java
+++ JuegoZone/src/conecta4/Main.java
@@ -11,5 +11,25 @@ package conecta4;
  */
 public class Main {
 
-    //TamaÃ±o del tablero
+    //Tamaño del tablero
+	static final int m_columnas=7;
+	static final int m_filas=6;
+	
+	//Tiempo de espera máximo para una jugada
+	static final int m_tiempoMaximo=2000000;
+	
+	/**
+	 * @param args the command line arguments
+	 * 
+	 */
+	 public static void main (Strings[args]){
+		//crea el juego
+		 Juego m_juego=new Juego(m_columnas,m_filas,m_tiempoMaximo,0,1);
+		 
+		 //crea e inicia la interfaz del juego
+		 Interfaz interfaz=new Interfaz(m_juego);
+		 interfaz. setVisible(true);
+	 }
+
+ }
    
