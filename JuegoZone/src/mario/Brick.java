81d654d48f6381cd1f15310486dbe1a51a5d9e26
Francisco Alcaraz <alcaraz1305@gmail.com>
2017-03-09 01:29:29 +0100

    Se han añadido nuevos métodos a la clase.

--- JuegoZone/src/mario/Brick.java
+++ JuegoZone/src/mario/Brick.java
@@ -1,22 +1,20 @@
-
 // Brick.java -------------------------------------------------------
 
 package mario;
-
 
 import java.applet.*;
 import java.awt.*;
 
 import recursosMario.Map;
 
-
 /**
  *
  */
 public class Brick extends WorldObject {
 
-	/* Permite que todos los objetos de esta misma clase
-	 * puedan pasar de una imagen a otra todos sincronizados.
+	/*
+	 * Permite que todos los objetos de esta misma clase puedan pasar de una
+	 * imagen a otra todos sincronizados.
 	 */
 	protected static int indexClass = 0;
 
@@ -26,11 +24,12 @@ public class Brick extends WorldObject {
 	/* Número de imágenes que representan al sprite. */
 	public static int LENGHT_IMAGES = 4;
 
-	/* Permite actualizar las imágenes para todos los objetos
-	 * de esa clase. */
+	/*
+	 * Permite actualizar las imágenes para todos los objetos de esa clase.
+	 */
 	public static void actClass() {
 		if (changeImg) {
-			indexClass = (indexClass+1)%LENGHT_IMAGES;
+			indexClass = (indexClass + 1) % LENGHT_IMAGES;
 			changeImg = false;
 		}
 	}
@@ -49,7 +48,7 @@ public class Brick extends WorldObject {
 	// el jugador no tiene la capacidad de romperlos,
 	// por lo tanto indica que el ladrillo se está moviendo.
 	protected boolean moving = false;
-	
+
 	// Velocidad del movimiento.
 	protected float movingSpeed = 1.5F;
 
@@ -64,16 +63,75 @@ public class Brick extends WorldObject {
 		setPreferredSize(Map.TILE_X_SIZE, Map.TILE_Y_SIZE);
 		setImages(imgNormal, 0, 4);
 		// Rectángulo para las colisiones
-		bounds.add(new Rectangle(-1, -1, width+1, height+1));
+		bounds.add(new Rectangle(-1, -1, width + 1, height + 1));
 		if (first) {
 			first = false;
 			indexAudio = 0;
 			audio = new AudioClip[5];
-			for (int i=0; i<audio.length; i++) {
-				audio[i] = stage.getSoundsLoader()
-					.getAudio("coin.wav", true, true); // TODO nombre de los sonidos
+			for (int i = 0; i < audio.length; i++) {
+				audio[i] = stage.getSoundsLoader().getAudio("coin.wav", true,
+						true); // TODO nombre de los sonidos
 			}
 		}
 	}
 
-	
+	public void act() {
+		if (moving) {
+			move();
+			speed.setY(speed.getAccurateY()-((Main)stage).getGravity());
+			if (y >= initY) {
+				speed.setY(0);
+				moving = false;
+			}
+		}
+		int frameFrec = (int)(stage.getFPS()/10);
+		if (frameFrec == 0
+		  || stage.getTotalUpdates()%frameFrec == 0) {
+			setImage(indexClass);
+			changeImg = true;
+		}
+	}
+
+	public void collision(Sprite s) {
+		if (s instanceof Player && supportsPlayer) {
+			Player p = (Player)s;
+			//Colisiones del eje X
+			if (getLeft().intersects(p.getRight())
+				&& p.getSpeed().getAccuratex() > 0) {
+				//System.out.println("Izquierda del brick");
+				p.getSpeed().setX(0);
+				p.setLeftWall((int)x);
+			} else if (getRight().intersects(p.getLeft())
+				&& p.getSpeed().getAccurateX() < 0) {
+					//System.out.println("Derecha del brick");
+					p.getSpeed().setX(0);
+					p.setRightWall((int)x+width);
+				}else
+				// Colisiones del eje Y
+				if (p.getHead().intersects(getFoot()) && p.isRising()) {
+					//System.out.println("Debajo del brick");
+					if (!moving) {
+						moving = true;
+						((Main)stage).getSoundsLoader().play("blockHit", false);
+						speed.setY(movingSpeed);
+						if (s.getSpeed().getAccurateY()>movingSpeed) {
+							s.getSpeed().setY(movingSpeed);
+						}
+						s.setY(y+height);
+					} else {
+					  s.getSpeed().setY(speed.getAccurateY());
+					}
+				} else if (p.getFoot().intersects(getHead())) {
+					if (!moving) {
+						//System.out.println("Arriba del brick");
+						p.setFloor((int)y);
+					   }
+				     }
+				   }
+				}
+
+	public void setY(float yPos) {
+		super.setY(yPos);
+		initY = yPos;
+	}
+}
