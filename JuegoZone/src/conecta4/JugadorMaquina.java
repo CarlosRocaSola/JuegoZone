5e6a3bac59b2bc45570e9cb5e8571bae68d768b4
Juan Rodriguez <informaticajuanrodriguez@gmail.com>
2017-03-10 20:06:28 +0100

    arreglo Jorge

--- JuegoZone/src/conecta4/JugadorMaquina.java
+++ JuegoZone/src/conecta4/JugadorMaquina.java
@@ -546,7 +546,7 @@ class tableroAux{
         	  		//Realiza una busqueda en horizontal
         	  		if(j+3 < this.columnas)
         	  			if ((this.matrix[i][j+1]==casilla) &&
-        	  			(this.matriz[i][j+2]==casilla) && (this.matrix[i][j+3]==casilla))
+        	  			(this.matrix[i][j+2]==casilla) && (this.matrix[i][j+3]==casilla))
         	  			{
         	  				ganador = casilla;
         	  				fin = true;
